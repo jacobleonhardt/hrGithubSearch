e312f372a2250396c5adc1971ce0f89d6c2ed6b8
leonhardt <jacob.leonhardt649@gmail.com>
2021-10-03 17:50:25 -0500

    basic style for profile results

--- frontend/hr-search/src/components/results/index.js
+++ frontend/hr-search/src/components/results/index.js
@@ -1,18 +1,23 @@
 import React, { useEffect } from 'react'
+import './results.css'
 
 const Results = ({ profile }) => {
 
     return (
-        <div>
-            <p>{profile.login}</p>
-            <img src={profile.avatar_url} />
-            <p>{profile.location}</p>
-            <p>{profile.name}</p>
-            <p>{profile.email}</p>
-            <p>{profile.public_repos}</p>
-            <p>{profile.created_at}</p>
-            <p>{profile.updated_at}</p>
-        </div>
+        <section className="profile">
+            <div className="left">
+                <img src={profile.avatar_url} />
+            </div>
+            <div className="right">
+                <h3>{profile.name} ({profile.login})</h3>
+                <p>{profile.location}</p>
+                {profile.email ? <p>{profile.email}</p>
+                    : <span className="no-info">No email listed.</span>}
+                <p>Public Repos: {profile.public_repos}</p>
+                <p>Member Since: {profile.created_at.slice(0,10)}</p>
+                <p>Active On: {profile.updated_at.slice(0,10)}</p>
+            </div>
+        </section>
     )
 }
 
